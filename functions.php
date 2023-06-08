b073d90844ac7d85d9ebc6b0a79327cb339a9729
GarySmithAlvarezVenturo <Garyalvarez@outlook.it>
2023-06-08 20:53:07 +0200

    Milestone 1-2 finish

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+// Funzione per generare una password casuale
+function generatePassword($length) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    $password = '';
+
+    // Genera la password casuale selezionando caratteri casuali dalla stringa $characters
+    for ($i = 0; $i < $length; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $password .= $characters[$index];
+    }
+
+    return $password;
+}
+?>
--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password Generator</title>
+    <title>Generatore di password</title>
+    <style>
+        form {
+            margin-bottom: 20px;
+        }
+    </style>
 </head>
 <body>
-    
+    <h1>Generatore di password</h1>
+
+    <form action="index.php" method="GET">
+        <label for="length">Lunghezza password:</label>
+        <input type="number" name="length" id="length" min="1" max="50" required>
+        <input type="submit" value="Genera password">
+    </form>
+
+    <?php
+    // Include il file con le funzioni
+    include 'functions.php';
+
+    // Controlla se è stato inviato il parametro "length" nel GET
+    if (isset($_GET['length'])) {
+        // Ottiene la lunghezza della password dal GET
+        $length = $_GET['length'];
+
+        // Genera la password casuale
+        $password = generatePassword($length);
+
+        // Stampa la password
+        echo '<p>La tua password generata è: ' . $password . '</p>';
+    }
+    ?>
 </body>
 </html>
